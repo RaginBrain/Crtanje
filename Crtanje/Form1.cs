ceeb2d0512f48c128848e58bf4b8f779343146a5
RaginBrain <inverted2logic@gmail.com>
2014-12-20 14:38:45 +0100

    Znaci donekle sam osposobio algoritam, dodao botun dva , cilu funkciju, grafički se vidi izvršavanje

--- Crtanje/Form1.cs
+++ Crtanje/Form1.cs
@@ -34,17 +34,133 @@ namespace Crtanje
         Graphics drawArea;
         Tocka A = new Tocka(new Point(30, 100));
         Tocka B = new Tocka(new Point(650, 100));
+        List<Tocka> put = new List<Tocka>();
 
-        public void Generiranje(Tocka prva, Tocka druga,int broj_tocaka, Graphics drawArea)
+        Tocka[] niz_Tocaka = new Tocka[15];
+        List<Tocka> list_Tocaka = new List<Tocka>();
+        
+
+
+      /*  public List<Tocka> Generiranje(List<Tocka> put,int broj_tocaka, Graphics drawArea)
         {
+            
+            List<Pravac> lista_crta = new List<Pravac>();
+            Tocka[] niz_Tocaka = new Tocka[broj_tocaka];
+            Random r = new Random();
+            Pen blackPen = new Pen(Color.Black);
+            //*******************************************
 
 
+            A.Draw(drawArea, blackPen);
+            B.Draw(drawArea, blackPen);
+
+            int udaljenost_AB = B.t.X - A.t.X;
+
+            Pravac p = new Pravac(A, B);
+            lista_crta.Add(p);
+
+            for (int i = 0; i < broj_tocaka; i++)
+            {
+
+                niz_Tocaka[i] = new Tocka(new Point(r.Next(A.t.X, B.t.X), r.Next(0, 260)));
+                lista_crta.Add(new Pravac(A, niz_Tocaka[i]));
+                lista_crta.Add(new Pravac(niz_Tocaka[i], B));
+                niz_Tocaka[i].Draw(drawArea, blackPen);
+            }
+
+
+
+
+
+            Tocka min = niz_Tocaka[0];
+            float mini = 200;
+            foreach (Tocka t in niz_Tocaka)
+            {
+                foreach (Tocka s in niz_tocaka_izbivenog_vektora)
+                    if (s.Distanca(t) < mini)
+                    {
+                        mini = s.Distanca(t);
+                        min = t;
+                    }
+            }
+
+
+
+        }*/
+
+
+
+        public List<Tocka> Rjesi(List<Tocka> put, List<Tocka> niz_Tocaka)
+        {
+            int pozicija = 0;
+            Tocka min = niz_Tocaka[0];
+            float mini = 2000;
+            Tocka A=null;
+            Tocka B=null;
+            for (int j = 0; j < put.Count - 1; j++)
+            {
+                A = put[j];
+                B = put[j+1];
+                Pravac p = new Pravac(A, B);
+                lista_crta.Add(p);
+
+                for (int i = 0; i < niz_Tocaka.Count(); i++)
+                {
+                    lista_crta.Add(new Pravac(A, niz_Tocaka[i]));
+                    lista_crta.Add(new Pravac(niz_Tocaka[i], B));
+                }
+
+
+
+                int udaljenost_AB = Math.Abs(B.t.X - A.t.X);
+                Tocka[] niz_tocaka_izbivenog_vektora = new Tocka[udaljenost_AB];
+
+
+                for (int i = 0; i < udaljenost_AB; i++)
+                {
+                    niz_tocaka_izbivenog_vektora[i] = new Tocka(new Point(A.t.X + i, (int)Prosjecni_Y(lista_crta, i, A)));
+                }
+
+
+
+
+                foreach (Tocka t in niz_Tocaka)
+                {
+                    foreach (Tocka s in niz_tocaka_izbivenog_vektora)
+                        if (s.Distanca(t) < mini)
+                        {
+                            mini = s.Distanca(t);
+                            min = t;
+                            pozicija = j+1;
+
+                        }
+                }
+            }
+            list_Tocaka.Remove(min);
+            
+            put.Insert(pozicija,min);
+
+            return put;
+            
+            
         }
 
         public Form1()
         {
             InitializeComponent();
             drawArea = pictureBox1.CreateGraphics();
+            put.Add(A);
+            put.Add(B);
+            Random r = new Random();
+
+            Pen blackPen = new Pen(Color.Black);
+
+            for (int i = 0; i < 15; i++)
+            {
+
+                list_Tocaka.Add ( new Tocka(new Point(r.Next(50, 600), r.Next(0, 260))));
+            }
+            
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -64,16 +180,14 @@ namespace Crtanje
             Tocka[] niz_Tocaka = new Tocka[15];
             
 
-            Tocka A = new Tocka(new Point(30, 100));
-            A.Draw(drawArea, blackPen);
 
-            Tocka B = new Tocka(new Point(650, 100));
+            A.Draw(drawArea, blackPen);
             B.Draw(drawArea, blackPen);
 
             Pravac p = new Pravac(A, B);
             lista_crta.Add(p);
             
-            for (int i = 0; i < 10; i++)
+            for (int i = 0; i < 15; i++)
             {
                
                 niz_Tocaka[i] = new Tocka(new Point(r.Next(50,600),r.Next(0,260)));
@@ -88,14 +202,33 @@ namespace Crtanje
             }
 
 
-            Tocka[] niz_tocaka_izbivenog_vektora = new Tocka[124];
-            for (int i = 0; i < 124; i++)
+            int udaljenost_AB = B.t.X - A.t.X;
+            Tocka[] niz_tocaka_izbivenog_vektora = new Tocka[udaljenost_AB];
+
+
+            for (int i = 0; i < udaljenost_AB; i++)
             {
-                niz_tocaka_izbivenog_vektora[i] = new Tocka(new Point(A.t.X+i * 5, (int)Prosjecni_Y(lista_crta,i*5,A)));
+                niz_tocaka_izbivenog_vektora[i] = new Tocka(new Point(A.t.X + i, (int)Prosjecni_Y(lista_crta, i, A)));
             }
+
+            Tocka min = niz_Tocaka[0];
+            float mini = 200;
+
+
+            foreach (Tocka t in niz_Tocaka)
+            {
+                foreach(Tocka s in niz_tocaka_izbivenog_vektora)
+                    if(s.Distanca(t)< mini)
+                    {
+                        mini=s.Distanca(t);
+                        min=t;
+                    }
+            }
+            min.DrawZuto(drawArea, blackPen);
 
             foreach (Tocka t in niz_tocaka_izbivenog_vektora)
             {
+                
                 t.DrawManje(drawArea, blackPen);
             }
 
@@ -113,6 +246,28 @@ namespace Crtanje
         private void pictureBox1_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+
+            drawArea.Clear(Color.White);
+            Pen blackPen = new Pen(Color.Black);
+            put=Rjesi(put, list_Tocaka);
+           Point[] niz_put = new Point[list_Tocaka.Count];
+ 
+            for(int i=0;i<put.Count-1;i++)
+            {
+                drawArea.DrawLine(blackPen, put[i].t, put[i + 1].t);
+                put[i].DrawZuto(drawArea, blackPen);
+            }
+
+            foreach (Tocka t in list_Tocaka)
+            {
+                t.Draw(drawArea, blackPen);
+            }
+          
         }
 
 
--- Crtanje/Tocka.cs
+++ Crtanje/Tocka.cs
@@ -14,6 +14,7 @@ namespace Crtanje
         Rectangle rect;
         Brush b;
         Brush green;
+        Brush zuti;
         public float x;
         public float y;
         
@@ -24,6 +25,7 @@ namespace Crtanje
             rect = new Rectangle(new Point(T.X-4,T.Y-4),new Size(8,8) );
             b= new SolidBrush(Color.Red);
             green = new SolidBrush(Color.Green);
+            zuti = new SolidBrush(Color.Yellow);
             x = T.X;
             y = T.Y;
 
@@ -41,8 +43,24 @@ namespace Crtanje
         {
             g.DrawArc(p, new Rectangle(rect.X,rect.Y,rect.Width-2,rect.Height-2), 0, 360);
             g.FillPie(green, rect, 0, 360);
+        }
+
+        public void DrawZuto(Graphics g, Pen p)
+        {
+            g.DrawArc(p, new Rectangle(rect.X, rect.Y, rect.Width - 2, rect.Height - 2), 0, 360);
+            g.FillPie(zuti, rect, 0, 360);
+        }
+        
 
 
+        public float Distanca(Tocka X)
+        {
+            double razlika_X = (X.t.X - t.X) * (X.t.X - t.X);
+            double razlika_y=(X.t.Y - t.Y) * (X.t.Y - t.Y);
+            double razlika=razlika_X+razlika_y;
+
+            double rez = Math.Sqrt( razlika );
+            return (float)rez;
         }
 
     }
